8d157c45f876d5e5b782986455b04f867995a319
mabiak <mabiane.polniak@gmail.com>
2022-03-24 10:45:15 -0300

    feat - get all talkers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,20 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Requisito 1
+
+const talker = 'talker.json';
+
+app.get('/talker', async (req, res) => {
+  try {
+    const data = await fs.readFile(talker, 'utf8');
+    res.status(200).json(JSON.parse(data));
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${talker}`);
+    console.log(err);
+  }
 });
 
 app.listen(PORT, () => {
